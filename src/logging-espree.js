c81657d6d03e369a4f6c06cdbf439b6b9ba54869
Thomas Edward <alu0101408248@ull.edu.es>
2023-03-02 14:04:11 +0000

    Basics added

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -4,13 +4,31 @@ import * as estraverse from "estraverse";
 import * as fs from "fs/promises";
 
 export async function transpile(inputFile, outputFile) {
-  // Fill in the code here
+  let input = await fs.readFile(inputFile, 'utf-8');
+  const output = addLogging(input);
+  if (outputFile === undefined) {
+    console.log(output);
+    return;
+  }
+  await fs.writeFile(outputFile, output);
 }
 
 export function addLogging(code) {
-  // Fill in the code here
+  const ast = espree.parse(code);
+  estraverse.traverse(ast, {
+    enter: function(node, parent) {
+      if (node.type === 'FunctionDeclaration' ||
+        node.type === 'FunctionExpression') {
+        addBeforeCode(node);
+      }
+    }
+  });
+  return escodegen.generate(ast);
 }
 
 function addBeforeCode(node) {
- // Fill in the code here
+  const name = node.id ? node.id.name : '<anonymous function>';
+  const beforeCode = "console.log('Entering " + name + "()');";
+  const beforeNodes = espree.parse(beforeCode).body;
+  node.body.body = beforeNodes.concat(node.body.body);
 }
---
+++ temp.txt
@@ -0,0 +1,9 @@
+function foo(a, b) {
+    console.log('Entering foo()');
+    var x = 'blah';
+    var y = function () {
+        console.log('Entering <anonymous function>()');
+        return 3;
+    }();
+}
+foo(1, 'wut', 3);
