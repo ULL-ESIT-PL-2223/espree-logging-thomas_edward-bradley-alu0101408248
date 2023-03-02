5c20e995d660d5737436af5726e19c529df0670f
Thomas Edward <alu0101408248@ull.edu.es>
2023-03-02 18:22:51 +0000

    Optimizations

--- entrada.js
+++ entrada.js
@@ -1,5 +1,5 @@
 function foo(a, b) { 
   var x = 'blah';   
-  var y = (e => { return 3 })();
+  var y = (() => { return 3 })();
 }     
 foo(1, 'wut', 3);
--- salida.js
+++ salida.js
@@ -1,8 +1,8 @@
 function foo(a, b) {
-    console.log('Entering foo(${ a }, ${ b }) At line 1');
+    console.log('Entering foo(${ a }, ${ b }) at line 1');
     var x = 'blah';
-    var y = (e => {
-        console.log('Entering <anonymous function>(${ e }) At line 3');
+    var y = (() => {
+        console.log('Entering <anonymous function>() at line 3');
         return 3;
     })();
 }
